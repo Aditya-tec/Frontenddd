145f7e2a84e2e5585af77ecc6daf2492ec546623
adii <zenowing3@gmail.com>
2024-01-14 18:18:54 +0530

    1:43:55 time

---
+++ src/components/cart-item.tsx
@@ -0,0 +1,40 @@
+import { Link } from "react-router-dom";
+import { FaTrash } from "react-icons/fa";
+type CartItemProps = {
+
+    cartItem: any;
+};
+
+export const CartItem = ({cartItem}: CartItemProps) => {
+  
+
+  const {photo, productId,name,price,quantity} = cartItem
+  
+    
+  
+  return (
+  <><div className="cart-item">
+
+          <img src={photo} alt="{name}" />
+          <article>
+              <Link to={`/product/${productId}`}>{name}</Link>
+
+              <span>
+                  {price}
+              </span>
+
+          </article>
+
+          <div>
+              <button>-</button>
+              <p>{quantity}</p>
+              <button>+</button>
+              <button>Remove</button>
+          </div>
+      </div>
+      <button>
+<FaTrash/>
+          </button></>
+  );
+};
+export default CartItem;
--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,6 +1,17 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import {VscError} from 'react-icons/vsc';
-const cartItems=[];
+import CartItem from '../components/cart-item';
+const cartItems = [
+   {
+    productId: "abcdef",
+    photo : "https://m.media-amazon.com/images/I/71TPda7cwUL._SX679_.jpg",
+    name: "macbook",
+    price :5678,
+    stock: 100,
+    quantity: 4,
+   },
+
+]
 const subtotal = 4000;
 const tax = Math.round(subtotal * 0.18);
 const shippingCharges = 300;
@@ -14,9 +25,32 @@ export const Cart = () => {
   const [couponCode , setCouponCode] = useState<string>("");
   const [isValidCouponCode , setsetIsValidCouponCode] = useState<boolean>(false);
 
+
+useEffect(() => {
+
+ const timeOutId = setTimeout(() => {
+   if(Math.random() > 0.5) setsetIsValidCouponCode(true);
+   else setsetIsValidCouponCode(false);
+     
+  }, 1000);
+
+
+  return () => {
+    clearTimeout(timeOutId);
+    setsetIsValidCouponCode(false);
+    };
+  } , [couponCode]);
+
   return (
     <div className="cart">
       <main>
+
+      {
+        cartItems.map((i, idx)=> (
+        <CartItem key= {idx} cartItem={i}/>
+        ))}
+
+        </main>
         <aside>
            <p>Subtotal : ₹ {subtotal}</p>
            <p>Shipping Chargess : ₹ {shippingCharges}</p>
@@ -36,19 +70,21 @@ Discount : <em> ₹ {discount} </em>
             onChange={(e) => setCouponCode(e.target.value)} 
             />
 
-{couponCode && isValidCouponCode? (
- <span>₹ {discount} off using <code> {couponCode}</code>
+{couponCode && (isValidCouponCode? (
+ <span className="green">
+  ₹ {discount} off using <code> {couponCode}</code>
   </span> 
-  ):  ( <span>
+  ):  ( 
+  <span className="red">
     INVALID!! <VscError/>
   </span>
 
-)}
+) )}
 
 
 
         </aside>
-      </main>
+     
 
 
     </div>
